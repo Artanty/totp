42bc2303fd53e88fef12f4bbadc379bd39913ca2
Artanty <thomasway7@gmail.com>
2026-09-06 23:10:27 +0300

    trig -d;

--- back/src/app.ts
+++ back/src/app.ts
@@ -20,7 +20,6 @@ export function buildApp(): FastifyInstance {
   app.decorate('prisma', prisma);
   app.register(authPlugin);
 
-  
   const apiPrefix = process.env.API_PREFIX ?? '/totp';
   app.register(authRoutes, { prefix: `${apiPrefix}/auth` });
   app.register(tokenRoutes, { prefix: `${apiPrefix}/tokens` });
