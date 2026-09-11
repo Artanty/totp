e6c442660a7852de603955ba69479f744a4da3a1
Artanty <thomasway7@gmail.com>
2026-09-11 21:04:03 +0300

    totp logout -d;

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,7 +4,6 @@ import { AppComponent } from './app/app.component';
 import { setTotpGateInjector } from './app/gate/gate.module';
 
 
-
 bootstrapApplication(AppComponent, appConfig)
   .then((appRef) => setTotpGateInjector(appRef.injector))
   .catch((err) => console.error(err));
