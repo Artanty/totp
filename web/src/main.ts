fc6c05aae3beac27012ed327e94f1424fa1e0d20
Artanty <thomasway7@gmail.com>
2026-09-11 12:39:23 +0300

    trig -d;

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,6 +3,8 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { setTotpGateInjector } from './app/gate/gate.module';
 
+
+
 bootstrapApplication(AppComponent, appConfig)
   .then((appRef) => setTotpGateInjector(appRef.injector))
   .catch((err) => console.error(err));
